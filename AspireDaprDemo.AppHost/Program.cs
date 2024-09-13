8f86bb6ec99e97b3fdb9c574b2d4130b2740f83a
Nico Vermeir <136470992+NVermeir@users.noreply.github.com>
2024-09-13 06:52:56 +0200

    adds dapr sidecar

--- AspireDaprDemo.AppHost/Program.cs
+++ AspireDaprDemo.AppHost/Program.cs
@@ -1,7 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Presentation_API>("presentation-api");
+builder.AddProject<Projects.Presentation_API>("presentation-api")
+    .WithDaprSidecar("external-api-dapr");
 
-builder.AddProject<Projects.UI>("ui");
+builder.AddProject<Projects.UI>("ui")
+    .WithDaprSidecar("external-ui-dapr");
 
 builder.Build().Run();
